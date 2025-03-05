86d44b85a19923eafa5b1f192c49dbae85aad449
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-03-05 16:46:38 -0300

    commit tipos de dados

---
+++ 2-variaveis/variaveis2.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "Variável 1"
+	fmt.Println(variavel1)
+
+}
---
+++ 3-tipos de dados/tipos-de-dados.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Code here
+	var numero int = 1000000000000000000
+	fmt.Println(numero)
+
+	var numero2 uint32 = 100000
+	fmt.Println(numero2)
+	// var numero3 int32 = 1000000000000000000
+	// INT32 = RUNE = INT32
+	// fmt.Println(numero3)
+	var numero3 rune = 123456
+	fmt.Println(numero3)
+
+	//byte = uint8
+	var numero4 byte = 123
+	fmt.Println(numero4)
+}
