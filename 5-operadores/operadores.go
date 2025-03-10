2ee7ddacb354ec4a340f6677d40690f9585a9ca1
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-03-10 12:02:28 -0300

    commit

--- 5-operadores/operadores.go
+++ 5-operadores/operadores.go
@@ -1,24 +1,30 @@
-package main	
+package main
 
-func main()  {
+import "fmt"
+
+func soma(a int, b int) int {
+	return a + b
+}
+func divisao(a int, int b) int {
+	if b == 0 {
+		panic("O denominador não pode ser 0")
+
+	}
+	return a / b
+}
+func multiplicacao(a int, b int) int {
+	return a * b
+}
+
+func main() {
 	// Operadores aritméticos
 	// + - * / %
 	// ++ --
 	// += -= *= /= %=
 	// == != > < >= <=
 	// && || !
+	fmt.Println(soma(3, 4))
+	fmt.Println(divisao(3, 4))
+	fmt.Println(multiplicacao(3, 4))
 
-	func soma(a int, b int) int {
-		return a + b
-	}
-	func divisao(a int, int b) int {
-		if b == 0 {	
-			panic("O denominador não pode ser 0")
-			
-		}
-		return a / b
-	}
-	func multiplicacao(a int, b int) int {
-		return a * b
-	}
- }
+}
