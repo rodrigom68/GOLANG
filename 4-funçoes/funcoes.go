fd803fe703526e148c3d154cea3215d47d7b147d
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-03-10 10:39:07 -0300

    commit

--- 4-funçoes/funcoes.go
+++ 4-funçoes/funcoes.go
@@ -4,12 +4,16 @@ func somar(a int, b int) int {
 	return a + b
 }
 
+func calculosMatematicos(a int, b int) (int, int) {
+	soma := a + b
+	subtracao := a - b
+	return soma, subtracao
+}
+
 func main() {
 	resultado := somar(3, 4)
 	println(resultado)
 
-	var f = func (somar) {
-		println(somar) {			
-		
-	}
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(3, 4)
+	println(resultadoSoma, resultadoSubtracao)
 }
