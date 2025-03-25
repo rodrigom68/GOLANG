116e99c5fe0f7d683ba4f83cc7fbcad2b50a63d2
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-03-24 22:26:23 -0300

    commit 10 maps

--- 10-Maps/maps.go
+++ 10-Maps/maps.go
@@ -2,25 +2,25 @@ package main
 
 import "fmt"
 
-func main() { 
-	
+func main() {
 
 	usuario := map[string]string{
-		"nome": "Pedro",
+		"nome":      "Pedro",
 		"sobrenome": "Silva",
 	}
 
-	fmt.Println(usuario["nome"]) 
+	fmt.Println(usuario["nome"])
 
 	usuario2 := map[string]map[string]string{
 		"nome": {
 			"primeiro": "João",
-			"segundo": "Pedro",
+			"segundo":  "Pedro",
 		},
 		"curso": {
-			"nome": "Engenharia",
+			"nome":      "Engenharia",
 			"faculdade": "USP",
 		},
+	}
 	fmt.Println(usuario2)
 	delete(usuario2, "nome")
 	fmt.Println(usuario2)
@@ -28,6 +28,7 @@ func main() {
 	usuario2["signo"] = map[string]string{
 		"nome": "Gêmeos",
 	}
+
 	fmt.Println(usuario2)
 
 }
