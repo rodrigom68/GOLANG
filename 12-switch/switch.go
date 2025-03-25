1d55d9476443927720a33b8b1b050439ede50e6c
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-03-25 00:48:39 -0300

    commit

--- 12-switch/switch.go
+++ 12-switch/switch.go
@@ -23,9 +23,35 @@ func diaDaSemana(numero int) string {
 	}
 }
 
+func diaDaSemana2(numero int) string {
+	var diaDaSemana string
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+	case numero == 2:
+		diaDaSemana = "Segunda-feira"
+	case numero == 3:
+		diaDaSemana = "Terça-feira"
+	case numero == 4:
+		diaDaSemana = "Quarta-feira"
+	case numero == 5:
+		diaDaSemana = "Quinta-feira"
+	case numero == 6:
+		diaDaSemana = "Sexta-feira"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "Número Inválido"
+	}
+	return diaDaSemana
+}
+
 func main() {
 	fmt.Println("Estrutura Switch")
 
 	dia := diaDaSemana(3)
 	fmt.Println(dia)
+
+	dia2 := diaDaSemana2(3)
+	fmt.Println(dia2)
 }
