9babd570577a9935b17fc2c7e596cbd3ed934810
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-04 11:17:43 -0300

    commit

--- 14-funçoes Avançadas/14.2 funcoes variaticas/variaticas.go
+++ 14-funçoes Avançadas/14.2 funcoes variaticas/variaticas.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
-func soma(numeros ...int) {
-	fmt.Println("Números:", numeros)
+func soma(numeros ...int) int {
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+
+	return total
+
+}
+
+func escrever(texto string, numeros ...int) {
+	fmt.Println(texto)
+	for _, numero := range numeros {
+		fmt.Println(texto, numero)
+	}
 }
 
 func main() {
-	soma(1, 2, 3, 4, 5)
+	totalDaSoma := soma(1, 2, 3, 4, 5)
+	fmt.Println(totalDaSoma)
+
+	escrever("Ola Mundo", 1, 2, 3, 4, 5)
 }
