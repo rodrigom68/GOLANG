fd4641170c3df91f93d2f0d580ecfcbd98aa207d
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-09 15:51:33 -0300

    commit

---
+++ 14-funçoes Avançadas/14.4-funcoes recursivas/recursivas.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci(posicao uint) uint {
+	if posicao <= 1 {
+		return posicao
+	}
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
+}
+
+func main() {
+	fmt.Println("Funcoes Recursivas")
+
+	// 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946
+
+	posicao := uint(10)
+	fmt.Println("Fibonacci de", posicao, "é", fibonacci(posicao))
+}
