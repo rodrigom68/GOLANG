f2f1f42d6f19efa0139edb7ff31eaae5112ecc74
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-10 02:27:18 -0300

    commit

--- 14-funçoes Avançadas/14.8-ponteiros/ponteiros.go
+++ 14-funçoes Avançadas/14.8-ponteiros/ponteiros.go
@@ -9,9 +9,17 @@ func inverterSinal(numero int) int {
 	return -numero
 }
 
+func inverterSinalPonteiro(numero *int) {
+	*numero = -(*numero) // Desreferenciação do ponteiro
+	// ou numero = -(*numero) // Não funciona, pois altera o valor do ponteiro, não o valor apontado
+}
+
 func main() {
 	numero := 20
 	numeInvertido := inverterSinal(numero)
 	fmt.Println("Número original:", numero)
 	fmt.Println("Número invertido:", numeInvertido)
+
+	novoNumero := 20
+	inverterSinalPonteiro(&novoNumero) // Passando o endereço de memória do número
 }
