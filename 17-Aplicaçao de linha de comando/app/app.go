983c2fb80c11beec3846e5ccc26c387e8e9a06ef
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-11 13:25:30 -0300

    commit

--- 17-Aplicaçao de linha de comando/app/app.go
+++ 17-Aplicaçao de linha de comando/app/app.go
@@ -8,6 +8,22 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"              // nome da aplicação
 	app.Usage = "Busca IPS e Nomes de Servidor na internet" // descrição da aplicação
 
-	return app // retorna a aplicação de linha de comando
+	app.Commands = []cli.Command{ // comandos da aplicação
+		{
+			Name:  "ip", // nome do comando
+			Usage: "Busca o IP de um servidor na internet", // descrição do comando
+			Flags: []cli.Flag{ // flags do comando
+				cli.StringFlag{
+					Name:  "servidor", // nome da flag
+					Value: "frigo-data.com.br", // valor padrão da flag				  
+				},
+			},
+			Action: buscarIps, // ação do comando
+		},
 
+	return app // retorna a aplicação de linha de comando
+}
+
+func buscarIps(c *cli.Context) { 	
+		
 }
