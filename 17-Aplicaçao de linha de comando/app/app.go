dbfc091ad7a414677afaed7c896dd6c54761a834
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-15 02:44:55 -0300

    commit

--- 17-Aplicaçao de linha de comando/app/app.go
+++ 17-Aplicaçao de linha de comando/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -14,21 +15,42 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"              // nome da aplicação
 	app.Usage = "Busca IPS e Nomes de Servidor na internet" // descrição da aplicação
 
+	flags := []cli.Flag{ // flags do comando
+		cli.StringFlag{
+			Name:  "host",              // nome da flag
+			Value: "frigo-data.com.br", // valor padrão da flag
+		},
+	},
+
 	app.Commands = []cli.Command{ // comandos da aplicação
 		{
 			Name:  "ip",                                    // nome do comando
-			Usage: "Busca o IP de um servidor na internet", // descrição do comando
-			Flags: []cli.Flag{ // flags do comando
-				cli.StringFlag{
-					Name:  "host",              // nome da flag
-					Value: "frigo-data.com.br", // valor padrão da flag
-				},
-			},
+			Usage: "Busca o IP de um servidor na internet",
+			flags: flags, // descrição do comando
 			Action: buscarIps, // ação do comando
 		},
+		{	
+			Name:  "servidores", // nome do comando
+			Usage: "Busca o nome de um servidores na internet",
+			flags: flags, // descrição do comando
+			Action: buscarServidores, // ação do comando
+		}
 	}
 
 	return app // retorna a aplicação de linha de comando
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host") // pega o valor da flag servidor
+
+	servidores, erro := net.LookupNS(host) // busca o nome do servidor na internet
+	if erro != nil {                        // se houver erro, imprime o erro
+		log.Fatal(erro) // registra o erro e encerra a aplicação
+	}
+
+	for _, servidor := range servidores { // para cada servidor encontrado
+		fmt.Println(servidor) // imprime o servidor
+	}
 }
 
 func buscarIps(c *cli.Context) {
