d60d4a1dd275d70ff4df107f76e7bfcc68e6e282
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-15 03:47:14 -0300

    commit

---
+++ 18-concorrencias/goroutines/gouroutines.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	escrever("Olá Mundo")
+	escrever("Programando em Go")
+}
+
+func escrever(texto string) {
+
+	for {
+		fmt.Println(texto)
+		time.Sleep(time.Second)
+	}
+}
