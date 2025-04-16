46b6f9dc1b1013c1f6eb414b1de3dbcdba1150bd
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-16 11:51:42 -0300

    commit

--- 18-concorrencias_goroutines/waitgroup/waitgroup.go
+++ 18-concorrencias_goroutines/waitgroup/waitgroup.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
+	var waitGroup sync.WaitGroup // Cria um WaitGroup
+
+	waitGroup.Add(2) // Adiciona duas goroutines ao WaitGroup
+
+	go func() {
+		escrever("Olá Mundo") // Goroutine
+		waitGroup.Done()      // Indica que a goroutine terminou
+	}()
+	go func() {
+		escrever("Programando em Go") // Goroutine
+		waitGroup.Done()              // Indica que a goroutine terminou
+	}()
+
+	waitGroup.Wait() // Aguarda todas as goroutines terminarem
+
 	escrever("Olá Mundo") // Goroutine
 	escrever("Programando em Go")
 }
