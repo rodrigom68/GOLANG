0f5f0609684c6c802df9a5dcb120a446e2010fa3
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-16 12:00:36 -0300

    commit

---
+++ 18-concorrencias/3-canais/canais.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := make(chan string)      // Cria um canal de string
+	go escrever("Olá Mundo", canal) // Goroutine
+
+	mensagem := <-canal   // Recebe a mensagem do canal
+	fmt.Println(mensagem) // Imprime a mensagem recebida
+}
+
+func escrever(texto string, canal chan string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(texto)
+		time.Sleep(time.Second)
+	}
+}
