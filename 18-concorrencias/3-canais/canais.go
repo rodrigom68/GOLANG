97fad44a9797da09ff848e407856883f0b059021
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-16 12:18:26 -0300

    commit

--- 18-concorrencias/3-canais/canais.go
+++ 18-concorrencias/3-canais/canais.go
@@ -9,11 +9,14 @@ func main() {
 	canal := make(chan string)      // Cria um canal de string
 	go escrever("Olá Mundo", canal) // Goroutine
 
+	fmt.Println("Depois da funçao escrever começar a ser executada") // Imprime mensagem antes de receber do canal
+
 	mensagem := <-canal   // Recebe a mensagem do canal
 	fmt.Println(mensagem) // Imprime a mensagem recebida
 }
 
 func escrever(texto string, canal chan string) {
+	time.Sleep(5 * time.Second) // Espera 5 segundos antes de enviar a mensagem
 	for i := 0; i < 5; i++ {
 		canal <- texto // Envia a mensagem para o canal
 		time.Sleep(time.Second)
