96aca79e727dd0cc7265b8cb7703c5b14591abda
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2025-04-16 21:35:26 -0300

    commit

--- 18-concorrencias/6-Padroes de Concorrencia/6.3-multiplexador/multiplexador.go
+++ 18-concorrencias/6-Padroes de Concorrencia/6.3-multiplexador/multiplexador.go
@@ -2,7 +2,25 @@ package main
 
 func main() {
 
+	canal := multiplexar(escrever("Olá Mundo"), escrever("Programando em Go")) // Cria um canal multiplexado
 
+
+}
+
+func multiplexar(canalDeEntrada1, canalDeEntrada2 <-chan string) <-chan string {
+	canalDesaida := make(chan string) 
+	go func() {
+		for {
+			select { // Seleciona o canal que está pronto para enviar ou receber
+			case mensagem := <-canalDeEntrada1:
+				canalDesaida <- mensagem // Envia a mensagem do canal1 para o canal multiplexado
+			case mensagem := <-canalDeEntrada2:
+				canalMultcanalDesaidaiplexado <- mensagem // Envia a mensagem do canal2 para o canal multiplexado
+			}
+		}
+	}()
+
+	return canalDesaida // Retorna o canal multiplexado
 }
 
 func escrever(texto string) <-chan string {
